355456c2e404538ef1a15637e12885cb20ba2180
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-09-27 19:43:29 +0300

    🪾 test_register_endpoint => 🛠️ update log system tests for more clear terminal and explicit messages of base assertion

--- apps/server/app/src/test/java/server/_lib_tests/MyAssrt.java
+++ apps/server/app/src/test/java/server/_lib_tests/MyAssrt.java
@@ -4,10 +4,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MyAssrt {
+
+    public static String buildStr(Object expected, Object received) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("🧪 expected =>%n");
+        sb.append(expected);
+        sb.append("❌ received =>%n");
+        sb.append(received);
+
+        return sb.toString();
+    }
+
     public static void assrt(ResT res, String msg, int status) {
         assertEquals(status, res.getStatus(),
-                String.format("❌ expected status %d • 🚦 received %d", status, res.getStatus()));
+                buildStr(status, res.getStatus()));
         assertTrue((res.getMsg()).contains(msg),
-                String.format("❌ expected msg %s • 📝 received %s", msg, res.getMsg()));
+                buildStr(msg, res.getMsg()));
     }
 }
