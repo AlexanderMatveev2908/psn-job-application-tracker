375e9a2a260ee4ef571cb3f53ea7030e2549eaff
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-09-28 23:42:41 +0300

    🪾 implment_jwt => 🛠️ patch

--- apps/server/java_pkg_cli/src/java_pkg_cli/lib/toml_catalog/mng_catalog.py
+++ apps/server/java_pkg_cli/src/java_pkg_cli/lib/toml_catalog/mng_catalog.py
@@ -1,6 +1,7 @@
 from argparse import Namespace
+from typing import cast
 from tomlkit import string, inline_table
-
+from tomlkit.items import InlineTable
 
 from java_pkg_cli.lib.reg import REG_VERSION
 from java_pkg_cli.lib.toml_catalog.ctx_catalog import CtxCatalog
@@ -11,6 +12,14 @@ def add_catalog(args: Namespace, ctx: CtxCatalog) -> None:
     splitted: list[str] = args.lib.split(":")
     toml_alias = to_toml_alias(splitted[1])
     gradle_alias = to_gradle_alias(splitted[1])
+
+    if gradle_alias in ctx.libs:
+        old_row = cast(InlineTable, ctx.libs[gradle_alias])
+
+        if 'ref' in old_row.get("version", {}):
+            ctx.versions.pop(toml_alias, None)
+
+            print(f"♻️ overriding existing version for => {gradle_alias}")
 
     row = inline_table()
     row["module"] = f"{splitted[0]}:{splitted[1]}"
