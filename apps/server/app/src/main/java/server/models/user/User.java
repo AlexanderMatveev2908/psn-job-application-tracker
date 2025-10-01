20525dbc0a3d5ef952c6796178570e13f366828c
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-10-01 19:12:52 +0300

    🪾 check_jwt => 🛠️ built jwt checkers to be part of baseMdw methods

--- apps/server/app/src/main/java/server/models/user/User.java
+++ apps/server/app/src/main/java/server/models/user/User.java
@@ -1,17 +1,22 @@
 package server.models.user;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import reactor.core.publisher.Mono;
+import server.decorators.flow.ErrAPI;
 import server.lib.security.hash.MyArgonHash;
 import server.models.RootTable;
 
-@Data
-@EqualsAndHashCode(callSuper = true)
-@Table("users")
+@Data @EqualsAndHashCode(callSuper = true) @Table("users")
 public class User extends RootTable {
 
     @Column("first_name")
@@ -35,8 +40,7 @@ public class User extends RootTable {
     public User() {
     }
 
-    public User(String firstName, String lastName, String email,
-            String password) {
+    public User(String firstName, String lastName, String email, String password) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
@@ -49,11 +53,43 @@ public class User extends RootTable {
     }
 
     public Mono<User> hashPwd() {
-        return MyArgonHash.rctHash(password)
-                .map(hash -> {
-                    setPassword(hash);
-                    return this;
-                });
+        return MyArgonHash.rctHash(password).map(hash -> {
+            setPassword(hash);
+            return this;
+        });
+    }
+
+    public Map<String, Object> forClient() {
+        Map<String, Object> data = new HashMap<>();
+
+        Class<?> curr = this.getClass();
+
+        while (curr != null && !curr.getName().startsWith("java.")) {
+            for (Field field : curr.getDeclaredFields()) {
+                int mods = field.getModifiers();
+
+                if (Modifier.isStatic(mods) || Modifier.isTransient(mods))
+                    continue;
+
+                field.setAccessible(true);
+
+                try {
+                    String key = field.getName();
+
+                    if (Stream.of("password", "totpSecret").anyMatch(sensitive -> sensitive.equals(key)))
+                        continue;
+
+                    Object val = field.get(this);
+                    data.put(key, val);
+
+                } catch (IllegalAccessException e) {
+                    throw new ErrAPI("err copying user to map", 500);
+                }
+            }
+            curr = curr.getSuperclass();
+        }
+
+        return data;
     }
 
 }
