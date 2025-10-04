95ff605e08d879f3adc932c4c4a763ae8f820025
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-10-04 13:45:18 +0300

    🪾 main => 🐛 fix check_style & spot_bug

--- apps/server/app/src/main/java/server/features/auth/paperwork/LoginForm.java
+++ apps/server/app/src/main/java/server/features/auth/paperwork/LoginForm.java
@@ -12,13 +12,13 @@ import server.paperwork.EmailCheck;
 @Data @EqualsAndHashCode(callSuper = true)
 public class LoginForm extends EmailCheck {
 
+    @NotBlank(message = "password required") @Pattern(regexp = Reg.PWD, message = "password invalid")
+    private final String password;
+
     public LoginForm(String email, String password) {
         super(email);
         this.password = password;
     }
-
-    @NotBlank(message = "password required") @Pattern(regexp = Reg.PWD, message = "password invalid")
-    private final String password;
 
     public static LoginForm fromMap(Map<String, Object> bd) {
         return new LoginForm((String) bd.get("email"), (String) bd.get("password"));
--- apps/server/app/src/main/java/server/middleware/security/CheckTokenMdw.java
+++ apps/server/app/src/main/java/server/middleware/security/CheckTokenMdw.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 import server.decorators.flow.Api;
@@ -17,7 +18,7 @@ import server.models.token.svc.TokenSvc;
 import server.models.user.User;
 import server.models.user.svc.UserSvc;
 
-@Service @RequiredArgsConstructor
+@SuppressFBWarnings({ "EI2" }) @Service @RequiredArgsConstructor
 public class CheckTokenMdw {
 
   private final UserSvc userSvc;
