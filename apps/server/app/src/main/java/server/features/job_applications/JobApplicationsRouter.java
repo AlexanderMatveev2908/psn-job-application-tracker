6e434dca16ce4b26bb60a003d419baa9da9e7b20
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-10-08 17:50:15 +0300

    🪾 create_application => 🛠️ _

--- apps/server/app/src/main/java/server/features/job_applications/JobApplicationsRouter.java
+++ apps/server/app/src/main/java/server/features/job_applications/JobApplicationsRouter.java
@@ -1,7 +1,6 @@
 package server.features.job_applications;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
