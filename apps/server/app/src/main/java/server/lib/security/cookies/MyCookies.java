702af726b40bac1a06597aa989bc9d13e020377c
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-10-08 18:14:05 +0300

    🪾 main => ⚠️ _

--- apps/server/app/src/main/java/server/lib/security/cookies/MyCookies.java
+++ apps/server/app/src/main/java/server/lib/security/cookies/MyCookies.java
@@ -1,25 +1,26 @@
 package server.lib.security.cookies;
 
-import java.time.Duration;
-
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Service;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import server.conf.env_conf.EnvKeeper;
 import server.conf.env_conf.etc.EnvMode;
+import server.lib.security.mng_tokens.expiry_mng.ExpMng;
 
 @Service @SuppressFBWarnings({ "EI2" })
 public class MyCookies {
     private final boolean isTest;
+    private final ExpMng expMng;
 
-    MyCookies(EnvKeeper envKeeper) {
+    MyCookies(EnvKeeper envKeeper, ExpMng expMng) {
         this.isTest = envKeeper.getEnvMode().equals(EnvMode.TEST);
+        this.expMng = expMng;
     }
 
     public ResponseCookie jweCookie(String clientToken) {
         return ResponseCookie.from("refreshToken", clientToken).httpOnly(true).secure(!isTest).path("/")
-                .maxAge(Duration.ofMinutes(15)).sameSite(isTest ? "Lax" : "None").build();
+                .maxAge(expMng.jwe().exp() + 60).sameSite(isTest ? "Lax" : "None").build();
     }
 
     public ResponseCookie delJweCookie() {
